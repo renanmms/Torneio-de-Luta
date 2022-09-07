1a00b631c5bfd42cba0693ccde3728851477a745
Renan Martins <renanmartins1999@hotmail.com>
2022-09-07 20:32:38 -0300

    Criando entidade Lutador

---
+++ Models/Lutador.cs
@@ -0,0 +1,13 @@
+﻿namespace Torneio_de_Luta.Models
+{
+    public class Lutador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public List<string> ArtesMarciais { get; set; }
+        public int Lutas { get; set; }
+        public int Vitorias { get; set; }
+        public int Derrotas { get; set; }
+    }
+}
