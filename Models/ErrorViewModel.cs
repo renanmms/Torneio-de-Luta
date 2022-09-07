27178ce3a0e7a820c1c0648b7e39d69954e014c5
Renan Martins <renanmartins1999@hotmail.com>
2022-09-07 12:33:15 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Torneio_de_Luta.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
