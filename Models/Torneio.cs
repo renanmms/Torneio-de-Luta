0da4d2c9724a05d90354482f6a400a509ffb30a1
Renan Martins <renanmartins1999@hotmail.com>
2022-09-10 16:19:30 -0300

    Adicionando classe Torneio

---
+++ Models/Torneio.cs
@@ -0,0 +1,12 @@
+﻿namespace Torneio_de_Luta.Models
+{
+    public class Torneio
+    {
+        private List<Lutador> _lutadores;
+        private Lutador Campeao;
+        public Torneio(List<Lutador> lutadores)
+        {
+            _lutadores = lutadores;
+        }    
+    }
+}
