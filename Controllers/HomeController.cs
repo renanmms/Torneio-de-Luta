c76a5d70b76e02b1892f1ce192fcf6403cbfd6f7
Renan Martins <renanmartins1999@hotmail.com>
2022-09-12 14:01:18 -0300

    Adicionando dados de teste

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,6 +36,35 @@ namespace Torneio_de_Luta.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Resultado()
+        {
+            var dadosDeTeste = new List<Lutador> {
+                new Lutador { Nome="Renan", Vitorias=32, Idade=22, Lutas=40, ArtesMarciais = new List<string>{ "Karate" } },
+                new Lutador { Nome="Luis", Vitorias=20, Idade=33, Lutas=30, ArtesMarciais = new List<string>{ "Jiu-Jitsu" } },
+                new Lutador { Nome="Rafael", Vitorias=40, Idade=18, Lutas=50, ArtesMarciais = new List<string>{ "Boxe" } },
+                new Lutador { Nome="Lucas", Vitorias=19, Idade=30, Lutas=55, ArtesMarciais = new List<string>{ "Muay-thai" } },
+                new Lutador { Nome="Thiago", Vitorias=10, Idade=20, Lutas=15, ArtesMarciais = new List<string>{ "Kung-Fu" } },
+                new Lutador { Nome="Rocky", Vitorias=15, Idade=25, Lutas=16, ArtesMarciais = new List<string>{ "Boxe" } },
+                new Lutador { Nome="Ryu", Vitorias=29, Idade=30, Lutas=30, ArtesMarciais = new List<string>{ "Karate", "Kung-Fu" } },
+                new Lutador { Nome="Kyo", Vitorias=20, Idade=32, Lutas=30, ArtesMarciais = new List<string>{ "Karate" } },
+                new Lutador { Nome="Akuma", Vitorias=99, Idade=150, Lutas=100, ArtesMarciais = new List<string>{ "Karate", "Kung-Fu" } },
+                new Lutador { Nome="Muhammad Ali", Vitorias=20, Idade=33, Lutas=30, ArtesMarciais = new List<string>{ "Boxe"} },
+                new Lutador { Nome="Anderson Silva", Vitorias=20, Idade=47, Lutas=30, ArtesMarciais = new List<string>{ "Boxe", "Jiu-jítsu", "Muay thai, Taekwondo", "Judô", "Capoeira" } },
+                new Lutador { Nome="Fulano", Vitorias=20, Idade=28, Lutas=30, ArtesMarciais = new List<string>{ "Sansho", "Jiu-Jitsu" } },
+                new Lutador { Nome="Sicrano", Vitorias=20, Idade=27, Lutas=30, ArtesMarciais = new List<string>{ "Boxe", "Kung-Fu" } },
+                new Lutador { Nome="Whindersson Nunes", Vitorias=27, Idade=33, Lutas=30, ArtesMarciais = new List<string>{ "Boxe" } },
+                new Lutador { Nome="Mike Tyson", Vitorias=20, Idade=56, Lutas=30, ArtesMarciais = new List<string>{ "Boxe" } },
+                new Lutador { Nome="Jon Jones", Vitorias=35, Idade=35, Lutas=60, ArtesMarciais = new List<string>{ "Boxe" , "Wrestling"} }
+            };
+
+            Torneio campeonato = new Torneio(dadosDeTeste);
+            campeonato.Iniciar();
+            var vencedor = campeonato.Campeao;
+
+            return View(vencedor);
+        }
+
         private static async Task<List<Lutador>> GetLutadores()
         {
             var lutadores = new List<Lutador>();
